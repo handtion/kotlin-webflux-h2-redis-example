3c1b7ebcfb9bfee1837b6da067870159f06d9e3b
handtion <danachaihand@gmail.com>
2021-03-31 17:49:27 +0700

    update redis cache category system

--- src/main/kotlin/com/example/reactive/repository/CategoryCustomReactiveRepository.kt
+++ src/main/kotlin/com/example/reactive/repository/CategoryCustomReactiveRepository.kt
@@ -4,6 +4,8 @@ import com.example.reactive.model.*
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 import java.util.stream.Collectors
 
 @Repository
@@ -35,8 +37,31 @@ class CategoryCustomReactiveRepository(val client: DatabaseClient) {
 
     }
 
+    fun findById(id: Int) : Flux<Category> {
+        return client.sql(""" 
+             SELECT C.*,P.* FROM CATEGORY C LEFT JOIN PRODUCT P ON C.CATEGORY_ID = P.PRODUCT_CATEGORY_ID
+             WHERE C.CATEGORY_ID = '$id'
+             ORDER BY C.CATEGORY_ID,P.PRODUCT_ID
+        """.trimIndent()).fetch().all()
+            .bufferUntilChanged { result -> result["category_id"] }
+            .map {
+                    list -> CategoryBuilder.build(
+                list[0]["category_id"] as Int,
+                list[0]["category_name"] as String,
+                list[0]["category_description"] as String,
+                list.stream().filter { it["product_id"]!=null }.map {
+                        item -> ProductBuilder.build(
+                    (item["product_id"] ?: 0) as Int,
+                    (item["product_name"] ?: "") as String,
+                    (item["product_description"] ?: "") as String,
+                    (item["product_price"] ?: 0.0) as Double,
+                    (item["product_category_id"] ?: 0) as Int
+                )
+                }.collect(Collectors.toList())
+            )
+            }
 
-
+    }
 
 }
 
--- src/main/kotlin/com/example/reactive/service/CategoryService.kt
+++ src/main/kotlin/com/example/reactive/service/CategoryService.kt
@@ -4,6 +4,7 @@ import com.example.reactive.model.Category
 import com.example.reactive.repository.CategoryCustomReactiveRepository
 import com.example.reactive.repository.CategoryReactiveRepository
 import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.deleteAndAwait
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -14,35 +15,34 @@ class CategoryService(val repository : CategoryReactiveRepository,
                       val customRepository : CategoryCustomReactiveRepository,
                       val redisOperations: ReactiveRedisOperations<String, Category>) {
 
-
-
     fun findAll() : Flux<Category> {
 
-        customRepository.findAll()
-            .flatMap {
-                //it.category_name = it.category_name + "_redis"
-                redisOperations.opsForValue().set("${it.category_id}",it)
-            }.subscribe()
-
-       return redisOperations.keys("*").flatMap(redisOperations.opsForValue()::get)//customRepository.findAll()
+       return redisOperations.keys("*").flatMap(redisOperations.opsForValue()::get)
+           .switchIfEmpty( customRepository.findAll() ).doOnNext {  redisOperations.opsForValue().set("${it.category_id}",it).subscribe() }
     }
 
     fun findById(id : Int) : Mono<Category> {
-        return redisOperations.keys("$id").flatMap(redisOperations.opsForValue()::get).toMono()//repository.findById(id)
+        return redisOperations.keys("$id").flatMap ( redisOperations.opsForValue()::get ).toMono()
+            .switchIfEmpty( customRepository.findById(id).toMono() ).doOnNext { redisOperations.opsForValue().set("${it.category_id}",it).subscribe() }
     }
 
     fun save(item : Mono<Category>) : Mono<Category>{
-        return item.flatMap { repository.save(it) }
+        return item.flatMap { repository.save(it) }.doOnNext { ii -> redisOperations.opsForValue().set("${ii.category_id}",ii).subscribe() }
     }
 
     fun delete(id : Int) : Mono<Void>{
+        redisOperations.opsForValue().delete("$id").subscribe()
         return repository.deleteById(id)
     }
 
     fun update(id : Int,item : Mono<Category>) : Mono<Category>{
        return item.flatMap { item ->
             val itemMono1: Mono<Category> = repository.findById(id)
-                .flatMap {  repository.save(Category(it.category_id,item.category_name,item.category_description)) }
+                .flatMap {
+                    val ite : Mono<Category> = repository.save(Category(it.category_id,item.category_name,item.category_description))
+                    redisOperations.opsForValue().set("${it.category_id}",item).subscribe()
+                    ite
+                }
             itemMono1
         }
     }
